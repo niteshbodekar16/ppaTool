4e0f072a712d93e4f9261d1575bb7092942e1968
Nitesh Bodekar <niteshbodekar16@gmail.com>
2019-10-03 12:12:51 +0530

    first commit

---
+++ ppmtool/src/main/java/com/ppmtool/java/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.ppmtool.java.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
